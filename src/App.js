09fd3f9b7e6c50b2a55373a33baa47d41673137e
roddazad <roddazad@gmail.com>
2025-05-23 06:27:10 -0700

    Installed react-router-dom and added the component and css folders to the src

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from '../src/pages/Home'
 
 function App() {
   return (
     <div className="App">
+      <Router>
+          <Routes>
+              <Route path ="/" element={<Home />}/>
+          </Routes>
+      </Router>
     </div>
   );
 }
